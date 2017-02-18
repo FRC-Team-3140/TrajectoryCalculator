57ffba37b027dbc9206c371919437a7eda63f660
rosha <rosha@DESKTOP-E8TNOVM.knoxschools.ad>
2017-02-18 15:03:56 -0500

    All of the paths and the model should be finalized.

--- src/usfirst/frc/team2168/robot/FalconPathPlanner.java
+++ src/usfirst/frc/team2168/robot/FalconPathPlanner.java
@@ -1024,22 +1024,22 @@ public class FalconPathPlanner
 		
 		//This is where you put in the coordinates of where you want the robot to move.
 		//It is from this that the program will derive the optimum trajectory.
-		double[][] MyPath = new double[][]{//Trajectory points you want the robot to go-to {x,y}
+		/*double[][] MyPath = new double[][]{//Trajectory points you want the robot to go-to {x,y}
 			{0,27-3.13-1-3.5/12}, //top left to gear
 			{7,22.6},
-			{(9.5+12.4)/2,(16.9+15.2)/2},
-		};
+			{(9.5+12.4)/2,(16.9+15.2)/2+.25},
+		};*/
 		
-		/*double[][] MyPath = new double[][]{//Trajectory points you want the robot to go-to {x,y}
-		{0,4},//left to gear
-		{10,4},
-		{(13+9.6)/2,(9.2+10.6)/2}, 
-	};  */
+		double[][] MyPath = new double[][]{//Trajectory points you want the robot to go-to {x,y}
+		{0,4.2},//left to gear
+		{7,4.2},
+		{(12.3+9.5)/2,(10.08+11.7)/2-.25}, 
+	};  
 	
 		/*double[][] MyPath = new double[][]{//Trajectory points you want the robot to go-to {x,y}
 			{0,13.5},//middle to gear
-			{9.5,(10.7+16.3)/2}
-		};  */
+			{9.5-.25,(10.7+16.3)/2}
+		}; */
 	
 		
 		long start = System.currentTimeMillis();
